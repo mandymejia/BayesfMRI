26c240270cee25b4f5ab6d7b3a14df4625e510e2
Mandy Mejia <mandy.mejia@gmail.com>
2020-07-30 20:20:13 -0400

    Created estimate_template.cifti

--- R/BayesGLM.R
+++ R/BayesGLM.R
@@ -7,8 +7,8 @@
 #' @param sphereR_fname File path of GIFTI-format right spherical surface (*.surf.gii) to use for resampling cifti data and gifti surfaces to lower resolution. Must be provided if GLM_method is "Bayesian" or "both" and resample is not NULL.
 #' @param brainstructures A vector indicating which brain structure(s) to model: 'left' (left cortical surface), 'right' (right cortical surface), and/or 'subcortical' (subcortical and cerebellar gray matter)
 # @param vol_regions A vector indicating which subcortical brain regions (3-21) to model. Default is to exclude brainstem (region 7). # vol_regions=c(3:6,8:21)
-#' @param wb_path (Optional) Path to Connectome Workbench folder or executable. 
-#'  If not provided, should be set with 
+#' @param wb_path (Optional) Path to Connectome Workbench folder or executable.
+#'  If not provided, should be set with
 #'  \code{ciftiTools.setOption("wb_path", "path/to/workbench")}.
 #' @param design A TxK task design matrix (or list of such matrices, for multiple-session modeling) with column names representing tasks. Each column represents the expected BOLD response due to each task, a convolution of the hemodynamic response function (HRF) and the task stimulus.  Must be provided if and only if onsets=NULL.  Note that the scale of the regressors will affect the scale and interpretation of the beta coefficients, so imposing a proper scale (e.g., set maximum to 1) is recommended.
 #' @param onsets A matrix of onsets (first column) and durations (second column) for each task in SECONDS, organized as a list where each element of the list corresponds to one task. Names of list should be task names. (Or for multi-session modeling, a list of such lists.)  Must be provided if and only if design=NULL.
@@ -197,10 +197,10 @@ BayesGLM <- function(cifti_fname,
 
     if(ss==1){
       cifti_ss <- read_cifti(
-        cifti_fname[ss], 
+        cifti_fname[ss],
         surfL_fname=surfL_fname, surfR_fname=surfR_fname,
         brainstructures=brainstructures,
-        resamp_res=resamp_res, 
+        resamp_res=resamp_res,
         sphereL_fname=sphereL_fname, sphereR_fname=sphereR_fname,
         write_dir=write_dir,
         wb_path=wb_path
@@ -209,9 +209,9 @@ BayesGLM <- function(cifti_fname,
       if(do_right) surf_right <- cifti_ss$SURF_RIGHT
     } else {
       cifti_ss <- read_cifti(
-        cifti_fname[ss], 
+        cifti_fname[ss],
         brainstructures=brainstructures,
-        resamp_res=resamp_res, 
+        resamp_res=resamp_res,
         sphereL_fname=sphereL_fname, sphereR_fname=sphereR_fname,
         write_dir=write_dir,
         wb_path=wb_path
@@ -658,3 +658,233 @@ BayesGLM_surface <- function(data, vertices = NULL, faces = NULL, mesh = NULL, m
   return(result)
 
 }
+
+#' Estimate INLA model
+#'
+#' @param formula Formula to put into inla
+#' @param data Dataset
+#' @param A Large, sparse observation matrix
+#' @param spde The spatial model, an object of class inla.spde
+#' @param prec_initial Initial precision
+#' @param num.threads Number of threads
+#' @param int.strategy INLA strategy for numerical integration.  "eb" (empirical Bayes) is recommended for computational efficiency, or "ccd" for greater accuracy
+#' @param verbose Logical indicating if should run in a verbose mode (default FALSE).
+#'
+#' @return Results from INLA
+#' @export
+#' @importFrom INLA inla
+#'
+#' @note This function requires the \code{INLA} package, which is not a CRAN package. See \url{http://www.r-inla.org/download} for easy installation instructions.
+#'
+estimate_model <- function(formula, data, A, spde, prec_initial, num.threads=4, int.strategy = "eb", verbose=FALSE){
+
+  result <- inla(formula, data=data, control.predictor=list(A=A, compute = TRUE),
+                 verbose = verbose, keep = FALSE, num.threads = num.threads,
+                 control.inla = list(strategy = "gaussian", int.strategy = int.strategy),
+                 control.family=list(hyper=list(prec=list(initial=prec_initial))),
+                 control.compute=list(config=TRUE)) #required for excursions
+  return(result)
+}
+
+
+#' Make Formula
+#'
+#' @param beta_names char vector of the names of each bbeta object in the environment
+#' @param repl_names char vector of the names of each replicate object in the environment
+#' @param hyper_initial Optional vector of initial values for hyperparameters of each latent field OR a list with each element corresponding to one column of the X matrix
+#'
+#' @return A formula representing the Bayesian GLM to be passed to `inla()`
+#'
+#' @importFrom stats as.formula
+#'
+#'
+make_formula <- function(beta_names, repl_names, hyper_initial=NULL){
+
+  # Example:
+  # beta_names = bbeta1, bbeta2, ...
+  # repl_names = repl1, repl2, ...
+  # formula: y ~ -1 + f(bbeta1, model = spde, replicate = repl1) + f(bbeta2, model = spde_sh, replicate = repl2)
+
+  # check length of beta_names, repl_names, hyper_initial
+
+  n_beta <- length(beta_names)
+
+  if(!is.null(hyper_initial)){
+    #if hyper_list provided is a vector, repeat it n_beta times as a list
+    if(!is.list(hyper_initial)){
+      hyper_initial <- rep(list(hyper_initial), n_beta)
+    }
+    hyper_vec <- paste0(', hyper=list(theta=list(initial=', hyper_initial, '))')
+  } else {
+    hyper_vec <- NULL
+  }
+
+  formula_vec <- paste0('f(',beta_names, ', model = spde, replicate = ', repl_names, hyper_vec, ')')
+  formula_vec <- c('y ~ -1', formula_vec)
+  formula_str <- paste(formula_vec, collapse=' + ')
+  return(formula_str)
+}
+
+#' Make data list to be passed to \code{estimate_model}
+#'
+#' @param y Vectorized BOLD data (all voxels, sessions, etc.)
+#' @param X List (length = number of sessions) of sparse design matrices size TVxVK from each session, each created using `organize_data()`
+#' @param betas List (length = number of tasks) of bbeta objects from organize_replicates
+#' @param repls List (length = number of tasks) of repl objects from organize_replicates
+#'
+#' @return List
+#'
+#' @importFrom Matrix bdiag
+#'
+#'
+make_data_list <- function(y, X, betas, repls){
+
+  # Check length/dimensions of y, X, elements of betas and repls all match
+  n_sess <- length(X)
+  nx <- length(betas) #check same as length(repls)
+  #figure out nvox
+  #check dim(X)
+  #check length of betas and repls
+
+  numel <- 1 + length(betas) + length(repls) + 1
+  model_data <- vector('list', length=numel)
+  names(model_data) <- c('y', 'X', names(betas), names(repls))
+  model_data$y <- y
+  model_data$X <- bdiag(X) #row/col structure: sess1_beta1, sess1_beta2, sess2_beta1, sess2_beta2, ...
+
+  nbeta <- length(betas)
+  for(i in 1:nbeta){
+    model_data[[2+i]] <- betas[[i]]
+    model_data[[2+nbeta+i]] <- repls[[i]]
+  }
+
+  return(model_data)
+}
+
+
+#' Extract Estimates of Activation
+#'
+#' @description Obtains the posterior mean or other summary statistic for each latent field
+#'
+#' @param object An object of class ‘"inla"’, a result of a call to inla
+#' @param session_names Vector of fMRI session names
+#' @param mask (Optional) Original mask applied to data before model fitting
+#' @param stat A string representing the posterior summary statistic to be returned
+#'
+#' @return Estimates from inla model
+#'
+extract_estimates <- function(object, session_names, mask=NULL, stat='mean'){
+
+  if(class(object) != "inla"){
+    stop("Object is not of class 'inla'")
+  }
+
+  res.beta <- object$summary.random
+  nbeta <- length(res.beta)
+  beta_names <- names(res.beta)
+
+  n_sess <- length(session_names)
+  n_loc <- length(res.beta[[1]]$mean)/n_sess #number of locations for which beta is estimated
+  if(!is.null(mask)) {
+    V <- length(mask)
+    if(sum(mask) != n_loc) warning('Number of nonzeros in mask does not equal the number of data locations in the model')
+  } else {
+    V <- n_loc
+  }
+  betas <- vector('list', n_sess)
+  names(betas) <- session_names
+
+  stat_names <- names(res.beta[[1]])
+  if(! (stat %in% stat_names) ) stop(paste0('stat must be one of following: ', paste(stat_names, collapse = ', ')))
+  stat_ind <- which(stat_names==stat)
+
+
+  for(v in 1:n_sess){
+    inds_v <- (1:n_loc) + (v-1)*n_loc #indices of beta vector corresponding to session v
+    betas_v <- matrix(NA, nrow=n_loc, ncol=nbeta)
+    colnames(betas_v) <- beta_names
+
+    for(i in 1:nbeta){
+      est_iv <- res.beta[[i]][[stat_ind]][inds_v]
+      betas_v[,i] <- est_iv
+    }
+    betas[[v]] <- matrix(NA, nrow=V, ncol=nbeta)
+    betas[[v]][mask==1,] <- betas_v
+  }
+  return(betas)
+}
+
+
+#' Extracts posterior density estimates for hyperparameters
+#'
+#' @param object An object of class ‘"inla"’, a result of a call to \code{inla()}
+#' @param spde The model used for the latent fields in the \code{inla()} call, an object of class ‘"inla.spde"’
+#' @param beta_names (Optional) Descriptive names of model regressors (tasks).
+#'
+#' @return Long-form data frame containing posterior densities for the hyperparameters associated with each latent field
+#'
+#' @importFrom INLA inla.spde2.result
+#'
+#' @note This function requires the \code{INLA} package, which is not a CRAN package. See \url{http://www.r-inla.org/download} for easy installation instructions.
+#'
+get_posterior_densities <- function(object, spde, beta_names=NULL){
+
+  beta_names_model <- names(object$summary.random)
+  numbeta <- length(beta_names_model)
+  if(numbeta != length(beta_names)) {
+    warning('Length of beta_names invalid.  Setting to NULL.')
+    beta_names <- NULL
+  }
+
+  for(b in 1:numbeta){
+    name_b = beta_names_model[b]
+    result.spde.b = inla.spde2.result(object, name_b, spde)
+    # Kappa and Tau
+    log_kappa.b = as.data.frame(result.spde.b$marginals.log.kappa$kappa.1)
+    log_tau.b = as.data.frame(result.spde.b$marginals.log.tau$tau.1)
+    names(log_kappa.b) <- names(log_tau.b) <- c('value','density')
+    log_kappa.b$param <- 'log_kappa'
+    log_tau.b$param <- 'log_tau'
+    df.b <- rbind(log_kappa.b, log_tau.b)
+    df.b$beta <- name_b
+    if(!is.null(beta_names)) df.b$name <- beta_names[b] else df.b$name <- NA
+    if(b == 1) df <- df.b else df <- rbind(df, df.b)
+  }
+  df <- df[,c('beta','name','param','value','density')]
+  return(df)
+}
+
+
+#' Extracts posterior density estimates for hyperparameters
+#'
+#' @param object An object of class ‘"inla"’, a result of a call to \code{inla()}
+#' @param spde The model used for the latent fields in the \code{inla()} call, an object of class ‘"inla.spde"’
+#'
+#' @return Long-form data frame containing posterior densities for the hyperparameters associated with each latent field
+#' @export
+#' @importFrom INLA inla.spde2.result
+#' @importFrom INLA inla.extract.el
+#'
+#' @note This function requires the \code{INLA} package, which is not a CRAN package. See \url{http://www.r-inla.org/download} for easy installation instructions.
+#'
+get_posterior_densities_vol3D <- function(object, spde){
+
+  hyper_names <- names(object$marginals.hyperpar)
+
+  for(h in hyper_names){
+    df.h <- inla.extract.el(object$marginals.hyperpar, h)
+    df.h <- as.data.frame(df.h)
+    names(df.h) <- c('x','y')
+    df.h$hyper_name <- h
+    df.h$beta_name <- ifelse(grepl('bbeta',h), #if bbeta appears in name
+                             gsub('.+bbeta','bbeta',h), #rename as bbeta*
+                             NA) #NA if this is the precision hyperparameter
+    df.h$theta_name <- ifelse(grepl('Theta',h), #if bbeta appears in name
+                              gsub(' for.+','',h), #rename as Theta*
+                              NA) #NA if this is the precision hyperparameter
+
+    if(h==hyper_names[1]) df <- df.h else df <- rbind(df, df.h)
+  }
+  return(df)
+}
+
--- R/estimate_template.R
+++ R/estimate_template.R
@@ -9,27 +9,29 @@
 #'
 #' @return
 #' @export
-#' @importFrom ciftiTools read_cifti_flat
+#' @importFrom ciftiTools read_cifti flatten_cifti
 #'
-template_estimate_cifti <- function(cifti_fnames, cifti_fnames2=NULL, GICA_fname, inds=NULL, brainstructures=c('left','right'), verbose=TRUE){
-  
-  GICA_fname <- '~/Dropbox/RESEARCH/TemplateICA/templates/HCP_surface_ICA25/HCP25.dtseries.nii'
+estimate_template.cifti <- function(cifti_fnames, cifti_fnames2=NULL, GICA_fname, inds=NULL, brainstructures=c('left','right'), verbose=TRUE){
+
+  if(verbose) cat('\n Reading in GICA result')
   GICA_cifti <- read_cifti(GICA_fname, brainstructures=brainstructures)
   #may need to save some info from GICA_cifti to map templates back to surfaces for visualization
 
-  #flatten GICA (any component may be NULL)
-  GICA <- GICA_cifti$CORTEX_LEFT 
-  GICA <- rbind(GICA, GICA_cifti$CORTEX_RIGHT) 
-  #GICA <- rbind(GICA, GICA_cifti$VOL) #pending version of ciftiTools with flat volumetric data
+  if(verbose) cat('\n Flattening GICA')
+  GICA <- flatten_cifti(GICA_cifti, brainstructures='everything') #Damon I know this might need to be changed if the brainstructures argument is not everything
   L0 <- ncol(GICA)
   V <- nrow(GICA)
-  wall_mask <- (is.nan(GICA[,1]) | rowSums(GICA)==0) #medial wall indicator
-  
+  wall_mask <- rep(FALSE, V)
+  wall_mask[is.nan(GICA[,1])] <- TRUE
+  wall_mask[is.na(GICA[,1])] <- TRUE
+  wall_mask[rowSums(GICA^2)==0] <- TRUE
+  GICA2 <- GICA[!wall_mask,]
+
   if(verbose){
     cat(paste0('\n Number of data locations: ',V))
     cat(paste0('\n Number of group ICs: ',L0))
   }
-  
+
   L <- L0
   if(!is.null(inds)){
     if(any(!(inds %in% 1:L0))) stop('Invalid entries in inds argument.')
@@ -37,36 +39,38 @@ template_estimate_cifti <- function(cifti_fnames, cifti_fnames2=NULL, GICA_fname
   } else {
     inds <- 1:L0
   }
-  
+
   N <- length(cifti_fnames)
-  
+
   if(verbose){
     cat(paste0('\n Number of template ICs: ',L))
     cat(paste0('\n Number of training subjects: ',N))
   }
-  
+
   retest <- !is.null(cifti_fnames2)
   if(retest){
     if(length(cifti_fnames) != length(cifti_fnames2)) stop('If provided, cifti_fnames2 must have same length as cifti_fnames and be in the same subject order.')
   }
-  
+
   # PERFORM DUAL REGRESSION ON (PSEUDO) TEST-RETEST DATA
   DR1 <- DR2 <- array(NA, dim=c(N, L, V))
+  missing_data <- 0
   for(ii in 1:N){
-    
+
+    if(verbose) cat(paste0('\n Reading in data for subject ',ii,' of ',N))
+
     #read in BOLD
     fname_ii <- cifti_fnames[ii]
-    BOLD_cifti_ii <- read_cifti(fname_ii, brainstructures=brainstructures)
-    
-    #flatten (any component may be NULL)
-    BOLD1_ii <- BOLD_cifti_ii$CORTEX_LEFT 
-    BOLD1_ii <- rbind(BOLD1_ii, BOLD_cifti_ii$CORTEX_RIGHT) 
-    #BOLD1_ii <- rbind(BOLD1_ii, BOLD_cifti_ii$VOL) #pending version of ciftiTools with flat volumetric data
-    rm(BOLD_cifti_ii) #Damon, can you see if this actually removes BOLD_cifti_ii from memory?
-    
-    if(nrow(BOLD1_ii) != V) stop(paste0('The number of data locations in GICA and timeseries data from subject ',ii,' do not match.'))
+    if(!file.exists(fname_ii)) {
+      missing_data <- missing_data + 1
+      if(verbose) cat(paste0('\n Data not available'))
+      next
+    }
+    BOLD1_ii <- read_cifti(fname_ii, brainstructures=brainstructures, flat=TRUE)
+
+    if(nrow(BOLD1_ii) != nrow(GICA2)) stop(paste0('The number of data locations in GICA and timeseries data from subject ',ii,' do not match.'))
     ntime <- ncol(BOLD1_ii)
-    
+
     #read in BOLD retest data OR create pseudo test-retest data
     if(!retest){
       part1 <- 1:round(ntime/2)
@@ -74,49 +78,51 @@ template_estimate_cifti <- function(cifti_fnames, cifti_fnames2=NULL, GICA_fname
       BOLD2_ii <- BOLD1_ii[,part2]
       BOLD1_ii <- BOLD1_ii[,part1]
     } else {
-      #read in BOLD from retest 
+      #read in BOLD from retest
       fname_ii <- cifti_fnames2[ii]
-      BOLD_cifti_ii <- read_cifti(fname_ii, brainstructures=brainstructures)
-      
-      #flatten 
-      BOLD2_ii <- BOLD_cifti_ii$CORTEX_LEFT 
-      BOLD2_ii <- rbind(BOLD2_ii, BOLD_cifti_ii$CORTEX_RIGHT) 
-      #BOLD2_ii <- rbind(BOLD2_ii, BOLD_cifti_ii$VOL) #pending version of ciftiTools with flat volumetric data
-      rm(BOLD_cifti_ii) #Damon, can you see if this actually removes BOLD_cifti_ii from memory?
+      if(!file.exists(fname_ii)) {
+        missing_data <- missing_data + 1
+        if(verbose) cat(paste0('\n Data not available'))
+        next
+      }
+      BOLD2_ii <- read_cifti(fname_ii, brainstructures=brainstructures, flat=TRUE)
     }
-    
+
     #perform dual regression on test and retest data
-    DR1_ii <- dual_reg(t(BOLD1_ii[!wall_mask,]), t(GICA[!wall_mask,]))$S
-    DR2_ii <- dual_reg(t(BOLD2_ii[!wall_mask,]), t(GICA[!wall_mask,]))$S
+    DR1_ii <- dual_reg(t(BOLD1_ii), t(GICA2))$S
+    DR2_ii <- dual_reg(t(BOLD2_ii), t(GICA2))$S
     DR1[ii,,!wall_mask] <- DR1_ii[inds,]
     DR2[ii,,!wall_mask] <- DR2_ii[inds,]
   }
-  
+
   # ESTIMATE MEAN
-  
+
+  if(verbose) cat('\n Estimating Template Mean')
   mean1 <- apply(DR1, c(2,3), mean, na.rm=TRUE)
   mean2 <- apply(DR2, c(2,3), mean, na.rm=TRUE)
-  
-  mean_meas1 = squeeze(nanmean(meas1,1));
-  mean_meas2 = squeeze(nanmean(meas2,1));
-  template_mean = (mean_meas1 + mean_meas2)/2;
-  
-  # ESTIMATE SIGNAL (BETWEEN-SUBJECT) VARIANCE 
-  
+  template_mean <- (mean1 + mean2)/2;
+
+  # ESTIMATE SIGNAL (BETWEEN-SUBJECT) VARIANCE
+
   # total variance
-  var_tot1 = squeeze(nanvar(meas1,0,1));
-  var_tot2 = squeeze(nanvar(meas2,0,1));
-  var_tot = (var_tot1 + var_tot2)/2;
-  
+  if(verbose) cat('\n Estimating Total Variance')
+  var_tot1 <- apply(DR1, c(2,3), var, na.rm=TRUE)
+  var_tot2 <- apply(DR2, c(2,3), var, na.rm=TRUE)
+  var_tot <- (var_tot1 + var_tot2)/2
+
   # noise (within-subject) variance
-  meas_diff = meas1 - meas2;
-  var_noise = (1/2)*squeeze(nanvar(meas_diff,0,1));
-  
+  if(verbose) cat('\n Estimating Within-Subject Variance')
+  DR_diff = DR1 - DR2;
+  var_noise <- (1/2)*apply(DR_diff, c(2,3), var, na.rm=TRUE)
+
   # signal (between-subject) variance
-  template_var = var_tot - var_noise;
-  template_var(template_var < 0) = 0;  
-  
-  
-  
-  
+  if(verbose) cat('\n Estimating Template (Between-Subject) Variance')
+  template_var <- var_tot - var_noise
+  template_var[template_var < 0] <- 0
+
+  template <- list(mean = template_mean, var = template_var)
+
+  return(template)
+
+
 }
