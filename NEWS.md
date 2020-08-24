412f14663db1568c6fd7b50b7167cdd4431f2c7b
Dan <danieladamspencer@users.noreply.github.com>
2020-08-24 15:23:01 -0400

    I need to change the structure of the vignette to run BayesGLM rather than BayesGLM_surface

--- NEWS.md
+++ NEWS.md
@@ -2,6 +2,8 @@
 
 * Updated `ciftiTools` function calls with new names based on `ciftiTools@1.3`.
 * Updated medial wall mask handling in `estimate_template.cifti` based on `ciftiTools@1.3` 
+* Started work on a vignette with simulated data
+* Bug fixes
 
 # 1.2
 
--- R/plot.BayesGLM.R
+++ R/plot.BayesGLM.R
@@ -1,6 +1,15 @@
+#' Plot results from a BayesfMRI object
+#'
+#' @param BayesGLM_object An object of class "BayesfMRI"
+#' @param mask The mask used to convert from cortical surface to volumetric data
+#' @param session_name The name of the session to plot the results from (defaults to the first session)
+#'
+#' @return Currently a list of final coefficient array images. This will change in future versions.
+#' @export
 plot.BayesGLM <- function(BayesGLM_object, mask, session_name = NULL) {
   # Create a conversion matrix
   in_binary_mask <- which(mask == 1, arr.ind = T)
+  in_binary_mask <- in_binary_mask[,2:1]
   convert_mat_A <- inla.spde.make.A(mesh = BayesGLM_object$mesh, loc = in_binary_mask)
   # Extract the point estimates
   if(is.null(session_name)) session_name <- BayesGLM_object$session_names[1]
@@ -17,4 +26,10 @@ plot.BayesGLM <- function(BayesGLM_object, mask, session_name = NULL) {
     geom_raster(aes(x = Var1, y = Var2, fill = value)) +
     scale_fill_gradient2("") +
     facet_grid(.~L1)
+  # Using Mandy's code
+  plot(mesh,rgl=TRUE, draw.edges=FALSE)
+  nColors <- 64
+  colindex <- as.integer(cut(BayesGLM_object$beta_estimates$single_session[,1],breaks=nColors))
+  plot(mesh,col=heat.colors(nColors)[colindex], rgl=TRUE)
+  return(coef_images)
 }
