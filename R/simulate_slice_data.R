f0df439aca6c2c346fc9537da31f7a59f2d17005
Dan <danieladamspencer@users.noreply.github.com>
2020-08-31 13:56:33 -0400

    Added simulate_slice_data

--- R/simulate_slice_data.R
+++ R/simulate_slice_data.R
@@ -2,13 +2,23 @@
 #'
 #' @param sessions a number
 #' @param num_tasks a number
-#' @param active_centers a matrix
+#' @param active_centers a matrix, with the number of rows corresponding to the
+#'   number of activation regions. There should be two columns, corresponding
+#'   to the x- and y-coordinates for the centers.
 #' @param active_size a vector of length \code{nrow(active_centers)}
-#' @param vary_active logical, if there are multiple sessions, should the activation centers vary between sessions?
+#' @param beta_weights a matrix, with the number of rows equal to
+#'   \code{num_tasks} and the number of columns equal to the number of rows in
+#'   \code{active_centers}
+#' @param vary_active logical, if there are multiple sessions, should the
+#'   activation centers vary between sessions?
 #' @param num_time length of the time series that is generated
 #' @param binary_template (optional) a binary brain slice image
 #'
-#' @return a list..
+#' @return A list in which the first element, \code{data}, is a list of
+#'   sessions, each with components \code{BOLD} (a matrix) and \code{design}
+#'   (a matrix). The second list element is named \code{betas}, which is a list
+#'   of sessions, each containing a list of the true values of the coefficients
+#'   used to generate the data to be analyzed.
 #' @export
 #' @importFrom neuRosim specifydesign specifyregion
 #'
@@ -19,6 +29,7 @@ simulate_slice_data <-
            num_tasks = 2,
            active_centers = NULL,
            active_size = NULL,
+           beta_weights = NULL,
            vary_active = T,
            num_time = 200,
            binary_template = NULL) {
@@ -37,7 +48,9 @@ simulate_slice_data <-
     return(task_n)
   }, simplify = T)
   # Create the responses
-  if(is.null(binary_template)) data("binary_template")
+  if(is.null(binary_template)) {
+    data("binary_template", package = "BayesfMRI",envir = environment())
+  }
   if(is.null(active_centers)){
     bin_dims <- dim(binary_template)
     active_centers <- rbind(
@@ -46,32 +59,52 @@ simulate_slice_data <-
       round(c(0.5,0.3) * bin_dims)
     )
   }
-  if(is.null(active_size)) active_size <- sample(2:4, size = nrow(active_centers))
+  if(is.null(active_size)) {
+    active_size <- sample(2:4, size = nrow(active_centers))
+  }
+  if(is.null(beta_weights)) {
+    largest_margin <- max(num_tasks, nrow(active_centers))
+    base_weights <- stats::toeplitz(rev(seq(largest_margin)) / largest_margin)
+    beta_weights <- base_weights[seq(num_tasks),seq(nrow(active_centers))]
+  }
   y_i <- sapply(seq(num_sessions), function(i) {
+    # Make the active regions
     activation_regions <- mapply(function(cs,ss) {
-      region_out <-
-        specifyregion(
-          dim = c(45, 54),
-          coord = cs + c(sample(seq(-3,3),size = 1),sample(seq(-3,3),size = 1)),
-          radius = ss,
-          form = "sphere",
-          fading = runif(1,0.1,0.5))
-      return(region_out * rgamma(1,20, 20))
-    },cs = split(active_centers, row(active_centers)),ss = sizes, SIMPLIFY = F)
-    # Create beta_1
-    beta_1 <- ifelse(binary_template == 0, NA, 0)
-    beta_1 <- beta_1 + Reduce(`+`,sapply(activation_regions[-3],`*`,
-                                         y = rgamma(1,20,20), simplify = F))
-    # Create beta_2
-    beta_2 <- ifelse(binary_template == 0, NA, 0)
-    beta_2 <- beta_2 + Reduce(`+`,sapply(activation_regions[-1],`*`,
-                                         y = rgamma(1,20,20), simplify = F))
-    # Create the mean response
-    task_1_means <- beta_1 %o% c(t1)
-    task_2_means <- beta_2 %o% c(t2)
-    y_means <- task_1_means + task_2_means
+      if(vary_active) {
+        region_out <-
+          neuRosim::specifyregion(
+            dim = c(45, 54),
+            coord = cs + c(sample(seq(-3,3),size = 1),sample(seq(-3,3),size = 1)),
+            radius = ss,
+            form = "sphere",
+            fading = runif(1,0.1,0.5))
+        region_out <- region_out * rgamma(1,20, 20)
+      } else {
+        region_out <-
+          neuRosim::specifyregion(
+            dim = c(45, 54),
+            coord = cs,
+            radius = ss,
+            form = "sphere",
+            fading = 0.3)
+      }
+      return(region_out)
+    },cs = split(active_centers, row(active_centers)),ss = active_size,
+    SIMPLIFY = F)
+    # Make the coefficients using the active regions
+    beta_coefficients <- sapply(seq(num_tasks), function(j) {
+      beta_components <- mapply(`*`, x = beta_weights[j,],
+                                y = activation_regions, SIMPLIFY = F)
+      beta <- Reduce(`+`, beta_components)
+      beta[binary_template == 0] <- NA
+      return(beta)
+    }, simplify = F)
+    # Make the mean values for the BOLD data
+    task_means <- mapply(`%o%`,X = beta_coefficients,
+                         Y = split(tasks, col(tasks)), SIMPLIFY = F)
+    y_means <- Reduce(`+`, task_means)
     # Use the mean responses to create the simulated response data
-    y_t <- apply(y_means, 1:2, function(bv) {
+    y_t <- apply(y_means, seq(length(dim(y_means)) - 1), function(bv) {
       if(is.na(bv[1])) {
         return(rep(NA,length(bv)))
       } else {
@@ -79,11 +112,16 @@ simulate_slice_data <-
         return(out)
       }
     })
+    # Remove any NA voxels and output the response as a matrix
     y <- apply(y_t,1, identity)
     y_exclude <- apply(y,1, function(yv) any(is.na(yv)))
     y <- y[!y_exclude,]
     y <- t(y)
-    return(list(y=y,betas = list(bbeta_1 = beta_1, bbeta_2 = beta_2)))
+    # Return everything
+    return(list(session = list(BOLD=y,design = tasks), betas = beta_coefficients))
   }, simplify = F)
-
+  names(y_i) <- paste("session",seq(num_sessions), sep ="_")
+  data <- sapply(y_i, `[[`, i = "session", simplify = F)
+  betas <- sapply(y_i, `[[`, i = "betas", simplify = F)
+  return(list(data = data, betas = betas))
 }
