b448841f5327186ec056c748b6f056b4817e3f5a
Mandy Mejia <mandy.mejia@gmail.com>
2021-02-09 11:15:46 -0500

    Update classicalGLM

--- R/classicalGLM.R
+++ R/classicalGLM.R
@@ -7,37 +7,28 @@
 #' @inheritParams scale_design_Param
 #' @inheritParams avg_sessions_Param
 #'
-#' @return A list of classical GLM task activation estimates, where each element represents a session.
+#' @return A list of lists containing classical GLM task activation estimates, standard error estimates, and degrees of freedom. Each list element represents a session.
 #'
-#' @importFrom Matrix solve crossprod
 #' @export
-classicalGLM <- function(data,
-                         scale_BOLD=TRUE,
-                         scale_design = TRUE,
-                         avg_sessions = TRUE){
+classicalGLM <- function(data, scale_BOLD=TRUE, scale_design = TRUE, avg_sessions = TRUE){
 
   #check that all elements of the data list are valid sessions and have the same number of locations and tasks
   if(!is.list(data)) stop('I expect data to be a list, but it is not')
   data_classes <- sapply(data, 'class')
   if(! all.equal(unique(data_classes),'list')) stop('I expect data to be a list of lists (sessions), but it is not')
-
   # Name sessions and check compatibility of multi-session arguments
   session_names <- names(data)
   n_sess <- length(session_names)
-
   if(n_sess == 1 & avg_sessions) avg_sessions <- FALSE
-
   V <- ncol(data[[1]]$BOLD) #number of data locations
   is_missing <- is.na(data[[1]]$BOLD[1,])
   V2 <- sum(!is_missing)
-
-  is_pw <- !is.matrix(session_data$single_session$design) #if prewhitening has been done, design is a large sparse matrix (class dgCMatrix)
+  is_pw <- !is.matrix(data$single_session$design) #if prewhitening has been done, design is a large sparse matrix (class dgCMatrix)
   if(is_pw){
     if(scale_BOLD | scale_design) stop('If data is prewhitened, scale_BOLD and scale_design should be FALSE. Setting both to FALSE.')
     scale_BOLD <- FALSE
     scale_design <- FALSE
   }
-
   #checks
   if(!is_pw){
     K <- ncol(data[[1]]$design) #number of tasks
@@ -55,21 +46,15 @@ classicalGLM <- function(data,
       if(ncol(data[[s]]$design) != K*V2) stop('All sessions must have the same number of tasks (columns of the design matrix), but they do not.')
     }
   }
-
-  if(avg_sessions) num_GLM <- n_sess + 1
+  if(avg_sessions) num_GLM <- n_sess + 1; session_names <- append(session_names,"avg")
   if(!avg_sessions) num_GLM <- n_sess
-
   GLM_result <- vector('list', length=num_GLM)
   names(GLM_result) <- session_names
-
   if(avg_sessions){
     y_reg_all <- X_reg_all <- NULL
   }
-
   for(s in 1:num_GLM){
-
     if(s <= n_sess){
-
       #scale data to represent % signal change (or just center to remove baseline if scale=FALSE)
       BOLD_s <- data[[s]]$BOLD
       if(sum(is_missing) > 0) BOLD_s <- BOLD_s[,!is_missing]
@@ -81,7 +66,6 @@ classicalGLM <- function(data,
         if(!is_pw) design_s <- scale(data[[s]]$design, scale=FALSE) #center design matrix to eliminate baseline
         if(is_pw) design_s <- data[[s]]$design
       }
-
       #regress nuisance parameters from BOLD data and design matrix (only for non-pw data, since pw data has already been nuisance regressed)
       do_nuisance <- ('nuisance' %in% names(data[[s]]))
       if(is_pw & do_nuisance) stop('Prewhitened data should not include nuisance. Contact developer.')
@@ -93,20 +77,16 @@ classicalGLM <- function(data,
         y_reg <- BOLD_s
         X_reg <- design_s
       }
-
       if(avg_sessions){
         y_reg_all <- rbind(y_reg_all, y_reg)
         X_reg_all <- rbind(X_reg_all, X_reg)
       }
     }
-
     if(s == n_sess + 1){
       y_reg <- y_reg_all
       X_reg <- X_reg_all
     }
-
     # ESTIMATE MODEL COEFFICIENTS
-
     beta_hat_s <- SE_beta_hat_s <- matrix(NA, K, V)
     if(is_pw){
       y_reg <- c(y_reg) #make y a vector
@@ -118,7 +98,6 @@ classicalGLM <- function(data,
       beta_hat_s[,!is_missing] <- coef_s #RHS is a vector
       resid_s <- matrix(y_reg - X_reg %*% coef_s, ncol = V2)
     }
-
     if(!is_pw){
       XTX_inv <- try(solve(t(X_reg) %*% X_reg))
       if("try-error" %in% class(XTX_inv)) stop("There is some numerical instability in your design matrix (due to very large or very small values). Scaling the design matrix is suggested.")
@@ -126,37 +105,23 @@ classicalGLM <- function(data,
       beta_hat_s[,!is_missing] <- coef_s #RHS is a matrix
       resid_s <- y_reg - X_reg %*% coef_s
     }
-
-
-
     # ESTIMATE STANDARD ERRORS OF ESTIIMATES
-
     ntime <- length(y_reg) / V2
     if(round(ntime) != ntime) stop('Error, contact developer')
-
     #compute residual SD
-    var_error <- colVars(resid_s) * (ntime - 1) / (ntime - K - 1)
+    var_error <- matrixStats::colVars(resid_s) * (ntime - 1) / (ntime - K - 1)
     if(is_pw) var_error <- rep(mean(var_error), length(var_error))
     sd_error <- sqrt(var_error)
-
     #compute SE of betas
-    SE_beta_s <- sqrt(diag(XtX_inv)) * rep(sd_error, each = K)
+    SE_beta_s <- sqrt(Matrix::diag(XTX_inv)) * rep(sd_error, each = K)
     SE_beta_hat_s[,!is_missing] <- SE_beta_s
-
     #DoF
     DOF <- ntime - K - 1
-
-
-
-
     GLM_result[[s]] <- list(estimates = beta_hat_s,
                             SE_estimates = SE_beta_hat_s,
                             DOF = DOF)
   }
-
-
   return(GLM_result)
-
 }
 
 #' Fit classical massive univariate GLM to task fMRI data after prewhitening
