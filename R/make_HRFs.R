3efb8da682a38f2a601a3ed20b63f9d6150c50de
Damon Pham <damondpham@gmail.com>
2022-06-10 22:06:38 -0400

    Use all HRFs, not just deriv (or second deriv)

--- R/make_HRFs.R
+++ R/make_HRFs.R
@@ -28,29 +28,37 @@ make_HRFs <- function(onsets, TR, duration, downsample=100, deriv = 0){
 
   nsec <- duration*TR; # Total time of experiment in seconds
   stimulus <- rep(0, nsec*downsample) # For stick function to be used in convolution
-  HRF_fn <- switch((deriv+1), HRF, dHRF, d2HRF)
-  HRF <- HRF_fn(seq(0, 30, by=1/downsample))[-1] #canonical HRF to be used in convolution
   inds <- seq(TR*downsample, nsec*downsample, by = TR*downsample) # Extract EVs in a function of TR
 
-  design <- matrix(NA, nrow=duration, ncol=K)
-  colnames(design) <- task_names
-  for(k in 1:K){
-    onsets_k <- onsets[[k]][,1] #onset times in scans
-    durations_k <- onsets[[k]][,2] #durations in scans
+  HRF_fn <- switch((deriv+1), HRF, dHRF, d2HRF)
+  HRF <- HRF_fn(seq(0, 30, by=1/downsample))[-1] #canonical HRF to be used in convolution
 
-    # Define stimulus function
-    stimulus_k <- stimulus
-    for(ii in 1:length(onsets_k)){
-      start_ii <- round(onsets_k[ii]*downsample)
-      end_ii <- round(onsets_k[ii]*downsample + durations_k[ii]*downsample)
-      stimulus_k[start_ii:end_ii] <- 1
+  design <- vector("list", length=deriv+1)
+
+  for (ii in seq(deriv+1)) {
+    dname_ii <- switch((deriv+1), "HRF", "dHRF", "d2HRF")
+    design[[ii]] <- matrix(NA, nrow=duration, ncol=K)
+    colnames(design[[ii]]) <- paste0(task_names, "_", dname_ii)
+
+    for(k in 1:K){
+      onsets_k <- onsets[[k]][,1] #onset times in scans
+      durations_k <- onsets[[k]][,2] #durations in scans
+
+      # Define stimulus function
+      stimulus_k <- stimulus
+      for(ii in 1:length(onsets_k)){
+        start_ii <- round(onsets_k[ii]*downsample)
+        end_ii <- round(onsets_k[ii]*downsample + durations_k[ii]*downsample)
+        stimulus_k[start_ii:end_ii] <- 1
+      }
+
+      # Convolve boxcar with canonical HRF & add to design[[ii]] matrix
+      HRF_k <- convolve(stimulus_k, rev(HRF), type='open')
+      design[[ii]][,k] <- HRF_k[inds]
     }
-
-    # Convolve boxcar with canonical HRF & add to design matrix
-    HRF_k <- convolve(stimulus_k, rev(HRF), type='open')
-    design[,k] <- HRF_k[inds]
   }
-
+  design <- do.call(cbind, design)
+  
   return(design)
 }
 
