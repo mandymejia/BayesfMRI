08f9bfc351ff82c13c881c3f09f96cd5baee905f
Damon Pham <damondpham@gmail.com>
2024-06-25 18:51:58 +0900

    Bring back "subcortical" brainstructure name

--- R/BayesGLM.R
+++ R/BayesGLM.R
@@ -176,22 +176,20 @@ BayesGLM <- function(
   }
 
   ### Brain structures. --------------------------------------------------------
-  if (!all(brainstructures %in% c("left","right","sub","all"))){
-    stop('`brainstructures` contains values other than "left","right","sub", or "all"')
+  if (!all(brainstructures %in% c("left","right","subcortical","all"))){
+    stop('`brainstructures` contains values other than "left","right","subcortical", or "all"')
   }
-  if ("all" %in% brainstructures) brainstructures <- c("left","right","sub")
+  if ("all" %in% brainstructures) brainstructures <- c("left","right","subcortical")
   if (is_xifti) {
-    has_bs <- c("left", "right", "sub")[!vapply(BOLD[[1]]$data, is.null, FALSE)]
+    has_bs <- c("left", "right", "subcortical")[!vapply(BOLD[[1]]$data, is.null, FALSE)]
     if(!all(brainstructures %in% has_bs)) stop("BOLD data does not contain all of the structures indicated in `brainstructures`")
+  } else {
+  
+    brainstructures <- fMRItools::match_input(
+      brainstructures, c("left","right","subcortical"),
+      user_value_label="brainstructures"
+    )
   }
-
-  # } else {
-  #
-  #   brainstructures <- fMRItools::match_input(
-  #     brainstructures, c("left","right","subcortical"),
-  #     user_value_label="brainstructures"
-  #   )
-  # }
   do$left <- ('left' %in% brainstructures)
   do$right <- ('right' %in% brainstructures)
   do$sub <- ('sub' %in% brainstructures)
